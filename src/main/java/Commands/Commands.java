8ecaab0c44b38f25cdd1f3dfbb74acf20ecec040
AxeR44 <alessandroalbini44@gmail.com>
2020-06-22 16:50:03 +0200

    Improving exception handling, first commit.

--- src/main/java/Commands/Commands.java
+++ src/main/java/Commands/Commands.java
@@ -1,5 +1,6 @@
 package Commands;
 
+import Exceptions.NotEnoughParametersException;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public interface Commands {
@@ -7,27 +8,27 @@ public interface Commands {
     void ping(GuildMessageReceivedEvent event);
     void invite(GuildMessageReceivedEvent event);
     void sendInfo(GuildMessageReceivedEvent event);
-    boolean sendTelegram(GuildMessageReceivedEvent event) throws Exception;
+    boolean sendTelegram(GuildMessageReceivedEvent event) throws NotEnoughParametersException;
     void listChannels(GuildMessageReceivedEvent event);
-    void play(GuildMessageReceivedEvent event, String Url, boolean hideNotification) throws NullPointerException;
+    void play(GuildMessageReceivedEvent event, String Url, boolean hideNotification) throws NullPointerException, NotEnoughParametersException;
     void skip(GuildMessageReceivedEvent event);
     void dequeue(String index, GuildMessageReceivedEvent event);
     void queue(GuildMessageReceivedEvent event);
     void translator(GuildMessageReceivedEvent event, String[] args);
-    void quiz(GuildMessageReceivedEvent event);
+    void quiz(GuildMessageReceivedEvent event) throws NotEnoughParametersException;
     void rJokes(GuildMessageReceivedEvent event);
     void languagePrinter(GuildMessageReceivedEvent event);
-    void voteKick(GuildMessageReceivedEvent event);
-    void survey(GuildMessageReceivedEvent event);
-    void endSurvey(GuildMessageReceivedEvent event);
-    void addTelegram(GuildMessageReceivedEvent event);
-    void removeTelegram(GuildMessageReceivedEvent event);
+    void voteKick(GuildMessageReceivedEvent event) throws NotEnoughParametersException;
+    void survey(GuildMessageReceivedEvent event) throws NotEnoughParametersException;
+    void endSurvey(GuildMessageReceivedEvent event) throws NotEnoughParametersException;
+    void addTelegram(GuildMessageReceivedEvent event) throws NotEnoughParametersException;
+    void removeTelegram(GuildMessageReceivedEvent event) throws NotEnoughParametersException;
     void coinToss(GuildMessageReceivedEvent event);
-    void reportUser(GuildMessageReceivedEvent event);
-    void softBan(GuildMessageReceivedEvent event);
-    void wikiResearch(GuildMessageReceivedEvent event);
-    void lyrics(GuildMessageReceivedEvent event);
-    void reminder(GuildMessageReceivedEvent event);
-    void rollDice(GuildMessageReceivedEvent event);
-    void seek(GuildMessageReceivedEvent event);
+    void reportUser(GuildMessageReceivedEvent event) throws NotEnoughParametersException;
+    void softBan(GuildMessageReceivedEvent event) throws NotEnoughParametersException;
+    void wikiResearch(GuildMessageReceivedEvent event)throws NotEnoughParametersException;
+    void lyrics(GuildMessageReceivedEvent event) throws NotEnoughParametersException;
+    void reminder(GuildMessageReceivedEvent event) throws NotEnoughParametersException;
+    void rollDice(GuildMessageReceivedEvent event) throws NotEnoughParametersException;
+    void seek(GuildMessageReceivedEvent event) throws NotEnoughParametersException;
 }
--- src/main/java/EventListener/Listener.java
+++ src/main/java/EventListener/Listener.java
@@ -2,6 +2,7 @@ package EventListener;
 
 import Commands.CommandsImpl;
 import Eventi.HiBotEvent;
+import Exceptions.NotEnoughParametersException;
 import Notifier.TelegramNotifierAsync;
 import Wrappers.ChannelList;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
@@ -30,117 +31,115 @@ public class Listener extends ListenerAdapter {
         if (!event.getAuthor().isBot()) {
             if (args[0].startsWith(prefix)) {
                 String rawCommand = args[0].substring(1);
-                switch (rawCommand) {
-                    case "ping":
-                        commands.ping(event);
-                        break;
-                    case "info":
-                        commands.sendInfo(event);
-                        break;
-                    case "invite":
-                        commands.invite(event);
-                        break;
-                    case "telegram":
-                        try {
+                try {
+                    switch (rawCommand) {
+                        case "ping":
+                            commands.ping(event);
+                            break;
+                        case "info":
+                            commands.sendInfo(event);
+                            break;
+                        case "invite":
+                            commands.invite(event);
+                            break;
+                        case "telegram":
                             if (commands.sendTelegram(event)) {
                                 event.getMessage().addReaction("\u2705").queue();
-                            } else {
-                                event.getMessage().addReaction("\u274C").queue();
                             }
-                        } catch (Exception e) {
-                            System.out.println("ERROR: " + e.getMessage());
-                            event.getChannel().sendMessage(e.getMessage()).queue();
-                        }
-                        break;
-                    case "help":
-                        commands.help(event);
-                        break;
-                    case "listGroups":
-                        commands.listChannels(event);
-                        break;
-                    case "play":
-                        try{
-                            commands.play(event, event.getMessage().getContentRaw().substring(6), false);
-                        }catch(NullPointerException e){
-                            event.getChannel().sendMessage(e.getMessage()).queue();
-                        }catch(StringIndexOutOfBoundsException e){
-                            event.getChannel().sendMessage("Numero di parametri invalido").queue();
-                        }
 
-                        break;
-                    case "leave":
-                        if(args.length == 1) {
-                            commands.play(event, null, false);
-                        } else {
-                            event.getChannel().sendMessage("Numero di parametri invalido. Vedi .help per la sintassi del comando").queue();
-                        }
-                        break;
-                    case "skip":
-                        commands.skip(event);
-                        break;
-                    case "dequeue":
-                        if(args.length == 2) {
-                            commands.dequeue(args[1], event);
-                        }else{
-                            event.getChannel().sendMessage("Numero di parametri invalido. Vedi .help per la sintassi del comando").queue();
-                        }
-                        break;
-                    case "queue":
-                        commands.queue(event);
-                        break;
-                    case "translate":
-                        commands.translator(event,args);
-                        break;
-                    case "mtt":
-                        commands.quiz(event);
-                        break;
-                    case "joke":
-                        commands.rJokes(event);
-                        break;
-                    case "langlist":
-                        commands.languagePrinter(event);
-                        break;
-                    case "votekick":
-                        commands.voteKick(event);
-                        break;
-                    case "survey":
-                        commands.survey(event);
-                        break;
-                    case "endSurvey":
-                        commands.endSurvey(event);
-                        break;
-                    case "addTelegram":
-                        commands.addTelegram(event);
-                        break;
-                    case "removeTelegram":
-                        commands.removeTelegram(event);
-                        break;
-                    case "coinToss":
-                        commands.coinToss(event);
-                        break;
-                    case "report":
-                        commands.reportUser(event);
-                        break;
-                    case "softban":
-                        commands.softBan(event);
-                        break;
-                    case "wiki":
-                        commands.wikiResearch(event);
-                        break;
-                    case "lyrics":
-                        commands.lyrics(event);
-                        break;
-                    case "reminder":
-                        commands.reminder(event);
-                        break;
-                    case "roll":
-                        commands.rollDice(event);
-                        break;
-                    case "seek":
-                        commands.seek(event);
-                        break;
+                            break;
+                        case "help":
+                            commands.help(event);
+                            break;
+                        case "listGroups":
+                            commands.listChannels(event);
+                            break;
+                        case "play":
+                            try {
+                                commands.play(event, event.getMessage().getContentRaw().substring(6), false);
+                            } catch (NullPointerException e) {
+                                event.getChannel().sendMessage(e.getMessage()).queue();
+                            } catch (StringIndexOutOfBoundsException e) {
+                                event.getChannel().sendMessage("Numero di parametri invalido").queue();
+                            }
+
+                            break;
+                        case "leave":
+                            if (args.length == 1) {
+                                commands.play(event, null, false);
+                            } else {
+                                event.getChannel().sendMessage("Numero di parametri invalido. Vedi .help per la sintassi del comando").queue();
+                            }
+                            break;
+                        case "skip":
+                            commands.skip(event);
+                            break;
+                        case "dequeue":
+                            if (args.length == 2) {
+                                commands.dequeue(args[1], event);
+                            } else {
+                                event.getChannel().sendMessage("Numero di parametri invalido. Vedi .help per la sintassi del comando").queue();
+                            }
+                            break;
+                        case "queue":
+                            commands.queue(event);
+                            break;
+                        case "translate":
+                            commands.translator(event, args);
+                            break;
+                        case "mtt":
+                            commands.quiz(event);
+                            break;
+                        case "joke":
+                            commands.rJokes(event);
+                            break;
+                        case "langlist":
+                            commands.languagePrinter(event);
+                            break;
+                        case "votekick":
+                            commands.voteKick(event);
+                            break;
+                        case "survey":
+                            commands.survey(event);
+                            break;
+                        case "endSurvey":
+                            commands.endSurvey(event);
+                            break;
+                        case "addTelegram":
+                            commands.addTelegram(event);
+                            break;
+                        case "removeTelegram":
+                            commands.removeTelegram(event);
+                            break;
+                        case "coinToss":
+                            commands.coinToss(event);
+                            break;
+                        case "report":
+                            commands.reportUser(event);
+                            break;
+                        case "softban":
+                            commands.softBan(event);
+                            break;
+                        case "wiki":
+                            commands.wikiResearch(event);
+                            break;
+                        case "lyrics":
+                            commands.lyrics(event);
+                            break;
+                        case "reminder":
+                            commands.reminder(event);
+                            break;
+                        case "roll":
+                            commands.rollDice(event);
+                            break;
+                        case "seek":
+                            commands.seek(event);
+                            break;
+                    }
+                }catch(NotEnoughParametersException e){
+                    event.getChannel().sendMessage(e.getMessage()).queue();
                 }
-            } else {
+            }else {
                 evnt.handle(event);
             }
         }
